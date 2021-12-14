375843af5a713cf3f8b0dd3bbd7aa0b555442509
ashMolly <a.ty.kto.13@gmail.com>
2021-12-14 11:14:15 +0300

    typo fix

--- PrimeNumber/Prime.cpp
+++ PrimeNumber/Prime.cpp
@@ -9,16 +9,10 @@ bool isPrime(int n)
 {
     if (n <= 1)
         return false;
-    if (n <= 3)
-        return true;
   
-    if (n % 2 == 0 || n % 3 == 0)
-        return false;
-  
-    for (int i = 5; i * i <= n; i = i + 6)
-        if (n % i == 0 || n % (i + 2) == 0)
+    for (int i = 2; i <= n / 2; i++)
+        if (n % i == 0)
             return false;
-  
     return true;
 }
 
@@ -33,9 +27,7 @@ int main()
 
     for (int i = 0; i < n; i++)
     {
-        if (isPrime(i)) {
-            storePrimes.push_back(i);
-        }
+        if (isPrime(i)) { storePrimes.push_back(i); }
     }
 
     for (int i = 0; i < storePrimes.size(); i++)
