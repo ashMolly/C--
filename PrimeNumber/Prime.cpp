bd5caeeebdb7f06792da12e4bbfce08647c6e2fc
ashMolly <a.ty.kto.13@gmail.com>
2021-12-14 10:47:42 +0300

    Prime fix

--- PrimeNumber/Prime.cpp
+++ PrimeNumber/Prime.cpp
@@ -5,6 +5,23 @@
 #include <vector>
 #include <math.h>
 
+bool isPrime(int n)
+{
+    if (n <= 1)
+        return false;
+    if (n <= 3)
+        return true;
+  
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+  
+    for (int i = 5; i * i <= n; i = i + 6)
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+  
+    return true;
+}
+
 int main()
 {
     int n, mp = 0;
@@ -14,32 +31,27 @@ int main()
     std::vector<int> storePrimes;
     std::vector<int> binaryRecord;
 
-    for (int i = 2; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
-        allNumbers.push_back(i);
-    }
-
-    for (int i = 2; i < allNumbers.size() / 2; i++)
-    {
-        if (allNumbers.at(i - 2) % i == 0)
-        {
-            storePrimes.push_back(allNumbers.at(i - 2));
+        if (isPrime(i)) {
+            storePrimes.push_back(i);
         }
     }
 
     for (int i = 0; i < storePrimes.size(); i++)
     {
-        binaryRecord.push_back(std::bitset<8>(storePrimes.at(i)).count());
-        for (int s = 1; s < binaryRecord.size() - 1; s++)
+        binaryRecord.push_back(8 - std::bitset<8>(storePrimes.at(i)).count());
+        for (int s = 1; s < binaryRecord.size(); s++)
         {
-            if (binaryRecord.at(s) > binaryRecord.at(s - 1))
+            if (binaryRecord.at(s) < binaryRecord.at(s - 1))
             {
-                mp = s;
+                mp = s - 1;
+                break;
             }
         }
     }
 
-    std::cout << "Простое число с наибольшим кол-вом нулей в двоичной записи: " << storePrimes.at(mp) << std::endl;
+    std::cout << "Простое число с наибольшим кол-вом нулей в двоичной записи: " << storePrimes.at(mp) << " [" << std::bitset<8>(storePrimes.at(mp)) << "]" << std::endl;
 
     return 0;
 }
